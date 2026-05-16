856f6a15f08a0543a7817c6115989c7e4ac0f721
LDPE888888 <78706776+LDPE888888@users.noreply.github.com>
2026-05-17 00:43:47 +0800

    Update jobClient.py

--- service/syncJob/jobClient.py
+++ service/syncJob/jobClient.py
@@ -473,7 +473,7 @@ class JobTask:
             # 如果是文件
             if not key.endswith('/'):
                 # 视频文件过滤
-                video_extensions = ('.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.ts', '.m2ts', '.mts', '.iso', '.f4v')
+                video_extensions = ('.mp4', '.mkv', '.avi', '.mov', '.ts', '.iso', '.cas')
                 is_video_file = any(key.lower().endswith(ext) for ext in video_extensions)
                 
                 if not is_video_file:
